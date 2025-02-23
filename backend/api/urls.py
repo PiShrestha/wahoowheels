f736ae1d35828a14b981e9099c2b73eb9e6ff4eb
huaden <161155310+huaden@users.noreply.github.com>
2025-02-22 22:39:22 -0500

    added ride form input boilerplate

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('notes/', views.NoteListCreate.as_view(), name='note-list'),
     path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='note-delete'),
-    
     path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
     
     path('rides/', views.RideListCreate.as_view(), name="ride-list"),
--- backend/api/views.py
+++ backend/api/views.py
@@ -16,7 +16,7 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from .models import CustomUser
+from .models import CustomUser, Note
 from .tokens import account_activation_token
 from django.shortcuts import redirect
 
